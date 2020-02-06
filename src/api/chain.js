552fb6e9c84e69ed1fda36e442c92e109c8030a3
Dan K <dkazenoff@gmail.com>
2020-02-06 01:52:42 -0500

    Neo4j integration/tests work

--- src/api/chain.js
+++ src/api/chain.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 // const _ = require('lodash');
 const chainModel = require('./../model/chainModel');
 const fetch = require('node-fetch');
@@ -8,8 +8,28 @@ const fetch = require('node-fetch');
 // xhr.open('GET', "https://ipinfo.io/json", true);
 	// xhr.send();
 	// xhr.onreadystatechange = processRequest;
+	// function processRequest(e) {
+	//     if (xhr.readyState == 4) {
+	//         // time to partay!!!
+	//     }
+	// }
 // var JSONbig = require('json-bigint');
+setInterval(print_test, 5000);
 
+async function print_test() {
+	console.log("hi")
+	let options = {
+		from: "100",
+		to: "500",
+		amount: 7.01012
+	}
+	let test_transaction = await chainModel.graph_test(options);
+
+	return true;
+	// res.status(200).json({
+ //    	transaction: test_transaction
+ //  });
+};
 
 
 router.post('/', async (req, res) => {
@@ -17,15 +37,11 @@ router.post('/', async (req, res) => {
 
 	
 
-	function processRequest(e) {
-	    if (xhr.readyState == 4) {
-	        // time to partay!!!
-	    }
-	}
+	
 
 
   res.status(200).json({
-    user: await chainModel.(req.body)
+    user: await chainModel.print_last_transaction(req.body)
   });
 });
 
--- src/model/chainModel.js
+++ src/model/chainModel.js
@@ -1,6 +1,27 @@
 const { creds } = require('./../../config/default');
-let neo4j = require('neo4j-driver').v1;
+let neo4j = require('neo4j-driver');
 let driver = neo4j.driver("bolt://0.0.0.0:7687", neo4j.auth.basic(creds.database.username, creds.database.password));
+
+exports.graph_test = async function(options) {
+    let session = driver.session();
+    //DO Payment API with Token. Await Confirmation*******************
+    let result = await session.run('CREATE (a:account {address: $address1})-[:SENT {value: $amount}]->(b:account {address: $address2}) return a', {
+        address1: options.from,
+        address2: options.to,
+        amount: options.amount
+
+    });
+    //let originalpearls = result.records[0].get(0).properties.prop;
+    //result.records[0].get(0).properties.prop = ;
+    session.close();
+    if (result.records.length >0) {
+    	console.log(`${options.from} sent ${options.amount} ETH to ${options.to}`)
+    	return result.records[0].get(0).properties.address;
+
+    }
+
+    return false;
+}
 
 exports.print_last_transaction = async function(options) {
 // var myDate = new Date("July 1, 1978 02:30:00"); // Your timezone!
